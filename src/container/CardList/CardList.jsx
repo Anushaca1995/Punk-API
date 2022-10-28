98623c79114e781afe9abbdc843442f873fb66af
Anusha Antony <anushaca1995@gmail.com>
2022-10-28 11:38:39 +0100

    Worked on some bugs

--- src/container/CardList/CardList.jsx
+++ src/container/CardList/CardList.jsx
@@ -1,6 +1,5 @@
 import "./CardList.scss";
 import Card from "../../components/Card/Card";
-import {Link} from "react-router-dom";
 
 const CardList = (props) =>{
     const {beersData, checkPH} = props;
