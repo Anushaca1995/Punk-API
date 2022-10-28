9ca834833d8fb972d34556d827980904d8951a7b
Anusha Antony <anushaca1995@gmail.com>
2022-10-28 12:05:48 +0100

    Added food pairing and brewer details into page

--- src/components/BeersInfo/BeersInfo.jsx
+++ src/components/BeersInfo/BeersInfo.jsx
@@ -11,7 +11,7 @@ const BeersInfo = () => {
   const [set, Setset]=useState(true);
   console.log(beerId);
   let url;
-  let name, description, image_url, tagline, abv, ph, first_brewed;
+  let name, description, image_url, tagline, abv, ph, first_brewed, food_pairing, brewers_tips;
   const getBeer = async () => {
    
     url = `https://api.punkapi.com/v2/beers/${beerId}`;
@@ -33,15 +33,21 @@ const BeersInfo = () => {
         abv=beer[0].abv;
         ph=beer[0].ph;
         first_brewed=beer[0].first_brewed;
+        food_pairing=beer[0].food_pairing.join(", ");
+        brewers_tips=beer[0].brewers_tips;
     }
    
   return (
     <article className="beer">
             <h1 className="beer__beerName">{name}</h1>
-            <img className="beer__img" src={image_url}/>
             <h2 className="beer__tag">{tagline}</h2>
+            <img className="beer__img" src={image_url}/>
             <p className="beer__desc">{description}</p>
-            <h2 className="beer__fact">Facts</h2>
+            <h2 className="beer__fact">Food Pairing</h2>
+            <p className="beer__desc">{food_pairing}</p>
+            <h2 className="beer__fact">Brewers Tips</h2>
+            <p className="beer__desc">{brewers_tips}</p>
+            <h2 className="beer__fact">More Beer Details</h2>
         <ul className="beer__fact-list">
           <li>ABV : {abv} </li>
           <li>PH Value : {ph} </li>
